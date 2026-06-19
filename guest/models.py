616f62a926c36eca1145a6b7f8331dd1f284f4c0
Якшин Константин Владимирович <k.yakshin@h1mf.net>
2026-06-19 15:28:23 +0300

    Конец задания

--- guest/models.py
+++ guest/models.py
@@ -4,8 +4,8 @@ from django.urls import reverse
 class Guest(models.Model):
     FullName = models.CharField(max_length=100, verbose_name="Полное имя")
     Birthday = models.DateField(verbose_name="Дата рождения")
-    GenderId = models.ForeignKey('gender.Gender', on_delete = models.CASCADE, verbose_name="Номер пола")
-    StatusId = models.ForeignKey('status.Status', on_delete = models.CASCADE, verbose_name="Номер статуса")
+    GenderId = models.ForeignKey('gender.Gender', on_delete = models.CASCADE, verbose_name="Пол")
+    StatusId = models.ForeignKey('status.Status', on_delete = models.CASCADE, verbose_name="Статус")
 
     def get_update_url(self):
         return reverse("guest_update", kwargs={"pk": self.pk})
